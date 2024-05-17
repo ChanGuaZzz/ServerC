f307f3610291ff17a1ee6c44037fc43cba04a11d
ChanGuaZzz <ggualdronarjona@gmail.com>
2024-05-17 14:07:07 +0200

    server

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ import { virtualAssistant } from "./controllers/Assistant.js";
 import { addAlimento, getDieta } from "./controllers/DietaController.js";
 import { modificar } from "./controllers/ModifyDatos.js";
 import { db } from "./config/db.js";
-import Redis from 'ioredis';
+import Redis from 'redis';
 import connectRedis from 'connect-redis';
 
 
