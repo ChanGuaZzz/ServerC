1111caac9746a576a8384269372e3210905d1ed7
ChanGuaZzz <ggualdronarjona@gmail.com>
2024-05-17 14:18:28 +0200

    server

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ app.use(
     saveUninitialized: true,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // 1 día en milisegundos
-      httpOnly: false,
+      httpOnly: true,
       secure: true, // Establece a true si estás usando HTTPS
     },
   })
