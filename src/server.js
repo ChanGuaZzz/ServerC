fd2b53ed7bc2403d6d23878e252b6bda1d0ad0b8
ChanGuaZzz <ggualdronarjona@gmail.com>
2024-05-17 14:57:44 +0200

    server

--- src/server.js
+++ src/server.js
@@ -35,7 +35,7 @@ app.use(
     proxy: true,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // 1 día en milisegundos
-      httpOnly: true,
+      httpOnly: false,
       secure: true, // Establece a true si estás usando HTTPS
       sameSite: 'none' 
     },
