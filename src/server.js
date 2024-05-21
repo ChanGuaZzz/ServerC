fd15a4b77e304d65ed5c243e77d2d472aef9db64
ChanGuaZzz <127145026+ChanGuaZzz@users.noreply.github.com>
2024-05-21 13:40:25 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ import Redis from 'redis';
 import connectRedis from 'connect-redis';
 
 // Configura Redis client
-const RedisStore = connectRedis(session);
+const RedisStore = connectRedis()
 /*
 const redisClient = Redis.createClient({
   host: process.env.RDHOST, // Cambia esto a la configuración de tu Redis
