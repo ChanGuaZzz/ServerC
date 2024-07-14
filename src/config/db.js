d1577149c5cc3e03642491626c4d4390c3f3bdb1
ChanguaZzz <ggualdronarjona@gmail.com>
2024-07-14 23:40:49 +0200

    gg

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,27 @@
 // config/db.js
-import mysql from "mysql";
+import { Client } from "pg";
 import Mongoose from "mongoose";
 
-const db = mysql.createConnection({
+// Configuración de PostgreSQL
+const db = new Client({
   host: process.env.DBHOST,
   port: process.env.DBPORT,
   user: process.env.DBUSER,
   password: process.env.DBPASS,
   database: process.env.DBNAME
 });
+
+// Conexión a PostgreSQL
+const connectPostgres = async () => {
+  try {
+    await db.connect();
+    console.log("Conexión exitosa a PostgreSQL");
+  } catch (error) {
+    console.error("Error de conexión a PostgreSQL:", error);
+  }
+};
+
+// Configuración de MongoDB
 const connectMongoDB = async () => {
   try {
     await Mongoose.connect(process.env.DB_URI);
@@ -18,4 +31,4 @@ const connectMongoDB = async () => {
   }
 };
 
-export { db, connectMongoDB };
+export { db, connectPostgres, connectMongoDB };
