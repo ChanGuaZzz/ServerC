e3f3667d05eac4cbe421576082603e6cbd1c492f
ChanguaZzz <ggualdronarjona@gmail.com>
2024-09-10 00:37:11 +0200

    CORS

--- src/server.js
+++ src/server.js
@@ -41,6 +41,14 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://controlz.onrender.com");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 
 
 
