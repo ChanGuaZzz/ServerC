852d405c3f6f8cd58cfe39c8614e6d905d2b7ad5
ChanguaZzz <ggualdronarjona@gmail.com>
2024-09-11 00:28:20 +0200

    Commit Gigante

--- src/config/db.js
+++ src/config/db.js
@@ -9,6 +9,46 @@ const db = mysql.createConnection({
   password: process.env.DBPASS,
   database: process.env.DBNAME
 });
+
+db.connect(err => {
+  if (err) {
+    console.error("Error al conectar a MySQL:", err);
+    setTimeout(() => {
+      connectMySQL(); // Reintentar conexión
+    }, 2000);
+  } else {
+    console.log("Conexión exitosa a MySQL");
+  }
+});
+
+db.on('error', err => {
+  console.error('Error en la conexión a MySQL:', err);
+  if (err.code === 'PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR') {
+    db.end(() => {
+      connectMySQL(); // Reconectar si ocurre un error fatal
+    });
+  }
+});
+
+const connectMySQL = () => {
+  db = mysql.createConnection({
+    host: process.env.DBHOST,
+    port: process.env.DBPORT,
+    user: process.env.DBUSER,
+    password: process.env.DBPASS,
+    database: process.env.DBNAME
+  });
+
+  db.connect(err => {
+    if (err) {
+      console.error("Error al reconectar a MySQL:", err);
+      setTimeout(connectMySQL, 2000); // Reintentar reconexión
+    } else {
+      console.log("Reconexión exitosa a MySQL");
+    }
+  });
+};
+
 const connectMongoDB = async () => {
   try {
     await Mongoose.connect(process.env.DB_URI);
